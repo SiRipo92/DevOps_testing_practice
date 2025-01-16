b9c4a342b5db1200125752ab5db92e50a606798d
SiRipo92 <ripochesierra@gmail.com>
2025-01-16 09:19:52 -0500

    Paramertized what NOT to see in the results and removed unnecessary step

--- labs/13_variable_substitution/features/steps/web_steps.py
+++ labs/13_variable_substitution/features/steps/web_steps.py
@@ -37,12 +37,8 @@ def step_impl(context, search_text):
     element = context.driver.find_element(By.ID, 'search_results')
     assert search_text in element.text
 
-@then('I should not see "Kitty" in the results')
-def step_impl(context):
+@then('I should not see "{search_text}" in the results')
+def step_impl(context, search_text):
     element = context.driver.find_element(By.ID, 'search_results')
-    assert "Kitty" not in element.text
+    assert search_text not in element.text
 
-@then('I should not see "Leo" in the results')
-def step_impl(context):
-    element = context.driver.find_element(By.ID, 'search_results')
-    assert "Leo" not in element.text
